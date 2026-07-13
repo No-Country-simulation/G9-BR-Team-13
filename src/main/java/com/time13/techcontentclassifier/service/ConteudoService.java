1396321c16bafee888d851f923373efe6bd24906
Carlos Vieira <c.gleysonti@gmail.com>
2026-07-13 20:18:33 -0300

    Salvar no banco

--- src/main/java/com/time13/techcontentclassifier/service/ConteudoService.java
+++ src/main/java/com/time13/techcontentclassifier/service/ConteudoService.java
@@ -2,17 +2,43 @@ package com.time13.techcontentclassifier.service;
 
 import com.time13.techcontentclassifier.dto.ConteudoRequestDTO;
 import com.time13.techcontentclassifier.dto.ConteudoResponseDTO;
+import com.time13.techcontentclassifier.entity.Conteudo;
+import com.time13.techcontentclassifier.repository.ConteudoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ConteudoService {
-    public ConteudoResponseDTO classificar(ConteudoRequestDTO request){
+    private final ConteudoRepository conteudoRepository;
+
+    public ConteudoService(ConteudoRepository conteudoRepository) {
+        this.conteudoRepository = conteudoRepository;
+    }
+    public ConteudoResponseDTO classificar(ConteudoRequestDTO request) {
+
+        // Simulação da classificação (depois será substituída pela IA)
+        String categoria = "Backend";
+        Double probabilidade = 0.98;
+        List<String> informacoes = List.of("Java", "Spring Boot", "API REST");
+
+        // Cria a entidade
+        Conteudo conteudo = Conteudo.builder()
+                .titulo(request.titulo())
+                .texto(request.texto())
+                .categoria(categoria)
+                .probabilidade(probabilidade)
+                .informacoesAdicionais(String.join(", ", informacoes))
+                .build();
+
+        // Salva no banco
+        conteudoRepository.save(conteudo);
+
+        // Retorna a resposta para o cliente
         return new ConteudoResponseDTO(
-                "Backend",
-                0.97,
-                List.of("Java", "Spring Boot", "API Rest")
+                categoria,
+                probabilidade,
+                informacoes
         );
     }
 }
