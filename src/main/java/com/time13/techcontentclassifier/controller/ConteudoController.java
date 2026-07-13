d44497a6050d0e5b0476dd62e9f44f75b9e00129
Carlos Vieira <c.gleysonti@gmail.com>
2026-07-12 21:36:57 -0300

    Criando as nossas exceptions e validações

--- src/main/java/com/time13/techcontentclassifier/controller/ConteudoController.java
+++ src/main/java/com/time13/techcontentclassifier/controller/ConteudoController.java
@@ -3,6 +3,7 @@ package com.time13.techcontentclassifier.controller;
 import com.time13.techcontentclassifier.dto.ConteudoRequestDTO;
 import com.time13.techcontentclassifier.dto.ConteudoResponseDTO;
 import com.time13.techcontentclassifier.service.ConteudoService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class ConteudoController {
     }
 
     @PostMapping
-    public ConteudoResponseDTO classificar(@RequestBody ConteudoRequestDTO request){
+    public ConteudoResponseDTO classificar(@Valid @RequestBody ConteudoRequestDTO request){
         return conteudoService.classificar(request);
     }
 }
