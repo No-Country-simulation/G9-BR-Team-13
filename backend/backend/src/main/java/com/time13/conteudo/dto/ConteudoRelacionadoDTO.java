73c5411f10a6898a2d557aa04ce70729f6e46761
Fabiana Cordeiro Rocha <fabianaf_cordeiro@hotmail.com>
2026-07-14 21:15:29 -0300

    Removido pasta backend duplicada

--- backend/backend/src/main/java/com/time13/conteudo/dto/ConteudoRelacionadoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.time13.conteudo.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record ConteudoRelacionadoDTO(
-        @JsonProperty("id_relacionado") String idRelacionado,
-        String titulo,
-        @JsonProperty("score_similaridade") Double scoreSimilaridade
-) {}
--- backend/backend/src/main/java/com/time13/conteudo/repository/ConteudoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.time13.conteudo.repository;
-
-import com.time13.conteudo.entity.Conteudo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConteudoRepository extends JpaRepository<Conteudo, Long> {
-}
